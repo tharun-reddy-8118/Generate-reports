69de190e7f8b2ccc19a94c8e8bb20aaa764fa979
tharun-reddy-8118 <141704173+tharun-reddy-8118@users.noreply.github.com>
2025-06-16 12:09:24 +0530

    Delete tempCodeRunnerFile.py

--- tempCodeRunnerFile.py
+++
@@ -1,7 +0,0 @@
-params = tuple([start_date, end_date] * (num_placeholders // 2))
-        # conn = get_db_connection()
-        # df = pd.read_sql_query(query, conn, params=params)
-        # conn.close()
-
-        # df.columns = ['Metric', 'AIG']
-        # table_html = df.to_html(index=False, border=1, classes="table", justify='center')
